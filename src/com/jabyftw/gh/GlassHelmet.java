fef4eb7bc7161ae1c75c8297ef2c51e880acd0de
Jabyftw <rfsantos1996@globo.com>
2014-01-15 12:19:57 -0200

    v0.1.1 - Fixed bugs that werent suposed to happen?

--- src/com/jabyftw/gh/GlassHelmet.java
+++ src/com/jabyftw/gh/GlassHelmet.java
@@ -1,5 +1,6 @@
 package com.jabyftw.gh;
 
+import java.util.logging.Level;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,22 +14,29 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Rafael
  */
 public class GlassHelmet extends JavaPlugin implements Listener {
-
+    
+    private final ItemStack glass = new ItemStack(Material.GLASS, 1);
+    
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        getLogger().log(Level.INFO, "Enabled!");
     }
-
+    
     @Override
     public void onDisable() {
     }
-
-    @EventHandler
+    
+    @EventHandler(ignoreCancelled = true)
     public void onInteract(PlayerInteractEvent e) {
         if (e.getAction().equals(Action.LEFT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             if (e.getClickedBlock().getType().equals(Material.GLASS)) {
-                if (e.getPlayer().getEquipment().getHelmet() == null) {
-                    e.getPlayer().getEquipment().setHelmet(new ItemStack(Material.GLASS, 1));
+                if (e.getPlayer().getInventory().getHelmet() == null) {
+                    e.getPlayer().getInventory().setHelmet(glass);
+                    e.getPlayer().sendMessage("§6New hat!");
+                    e.setCancelled(true);
+                }
+                if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
                     e.setCancelled(true);
                 }
             }
